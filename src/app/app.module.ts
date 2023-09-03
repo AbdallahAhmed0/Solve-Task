8dd177e4f515823bebafd3d8b5d3a1c0ec9a061d
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-09-03 22:31:19 +0300

    add  component graph

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Graph1Component } from './Dashboard/graph1/graph1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Graph1Component
   ],
   imports: [
     BrowserModule
