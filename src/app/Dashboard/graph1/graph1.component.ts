02bac51012b51eb6d32fdc06a596e106741b8d90
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-09-04 09:16:40 +0300

    add images

--- src/app/Dashboard/graph1/graph1.component.ts
+++ src/app/Dashboard/graph1/graph1.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-graph1',
   templateUrl: './graph1.component.html',
-  styleUrls: ['./graph1.component.css']
+  styleUrls: ['./graph1.component.css'],
 })
-export class Graph1Component {
-  
-}
+export class Graph1Component { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { Graph1Component } from './Dashboard/graph1/graph1.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './Dashboard/header/header.component';
+import { SidebarComponent } from './Dashboard/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Graph1Component
+    Graph1Component,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
