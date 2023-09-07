31d857ac6f736fbadc6ab80cbd43f844bfa353c0
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-09-07 08:04:12 +0300

    edit chart bar

--- src/app/Dashboard/graph1/graph1.component.ts
+++ src/app/Dashboard/graph1/graph1.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ChartConfiguration} from 'chart.js';
+import { ChartConfiguration, ChartData, ChartOptions} from 'chart.js';
 
 @Component({
   selector: 'app-graph1',
@@ -8,19 +8,29 @@ import { ChartConfiguration} from 'chart.js';
 })
 export class Graph1Component {
   public barChartLegend = true;
-  public barChartPlugins = [];
+  public barChartData: ChartData<'bar'> = {
+    labels: ['Station Name Dummy 1','Station Name Dummy 1', 'Station Name Dummy 2', 'Station Name Dummy 3', 'Station Name Dummy 4'],
 
-  public barChartData: ChartConfiguration<'bar'>['data'] = {
-    labels: ['Station Name Dummy 1','Station Name Dummy 1',
-     'Station Name Dummy 1','Station Name Dummy 1', 'Station Name Dummy 1', 'Station Name Dummy 1',
-      'Station Name Dummy 1', 'Station Name Dummy 1'],
     datasets: [
-      { data: [ 255, 145, 200,100,350,25, 245, 145], label: 'Series A' },
-    ]
+      {
+        data: [255, 145, 200, 245,141],
+        label: 'STATIONS ACTIVATES OVERVIEW',
+        backgroundColor: '#FF7F5C',
+        barPercentage: 0.5, // Adjust the width of the bars (0.5 means 50% of the available space)
+        categoryPercentage: 0.18, // Adjust the space between bars (0.8 means 80% of the available space)
+      },
+    ],
   };
 
-  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
-    responsive: false,
+  public barChartOptions: ChartOptions<'bar'> = {
+    responsive: true, // Enable responsiveness
+    maintainAspectRatio: false, // Prevent the chart from maintaining a fixed aspect ratio
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+    },
   };
-
-}
+  }
--- src/app/Dashboard/sidenav/sidenav.component.ts
+++ src/app/Dashboard/sidenav/sidenav.component.ts
@@ -9,12 +9,24 @@ import { ChartConfiguration } from 'chart.js';
 export class SidenavComponent {
   public radarChartOptions: ChartConfiguration<'radar'>['options'] = {
     responsive: false,
+      layout: {
+          padding: 2
+  }
   };
   public radarChartLabels: string[] = ['24:00', '2:00', '4:00', '6:00', '8:00', '10:00', '12:00',
                                         '14:00', '16:00', '18:00', '20:00', '22:00'];
 
   public radarChartDatasets: ChartConfiguration<'radar'>['data']['datasets'] = [
-    { data: [18, 59, 90, 10, 56, 20, 40,28, 48, 75, 19,56], label: '' },
-    { data: [28, 48, 70, 19, 96, 27, 100, 81, 56, 55, 40,89], label: '' }
+    {
+    data: [18, 59, 90, 10, 56, 20, 40,28, 48, 75, 19,56],
+    label: '' ,
+    backgroundColor: '#009ACF33',
+    borderColor:'#009ACF',
+
+  },
+    { data: [28, 48, 70, 19, 96, 27, 100, 81, 56, 55, 40,89],
+    label: '',
+    backgroundColor: '#009ACF33',
+    borderColor:'#009ACF'}
   ];
 }
