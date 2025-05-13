ac7f8c69dd6b70eadc905d3790bdd4b156c35d1f
AbdallahAhmed <abdallahahmadadel@gmail.com>
2025-05-13 17:32:03 +0300

    add ltr and rtl in tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
+  darkMode: 'class', // or 'media'
   theme: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    textAlign: false, // Disable default textAlign utilities
+  },
+  variants: {
+    extend: {
+      textAlign: ['rtl'], // Enable RTL variants for textAlign
+    },
+  }
 }
