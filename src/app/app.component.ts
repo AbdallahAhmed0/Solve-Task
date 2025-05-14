502f0df6457b10613c8bc188f0c8ddd99031dce7
AbdallahAhmed <abdallahahmadadel@gmail.com>
2025-05-14 19:50:58 +0300

    add animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ParentComponent } from "./components/parent/parent.component";
 import { TranslateService } from '@ngx-translate/core';
+declare const AOS: any; // 👈 Tell TypeScript about global AOS
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,19 @@ import { TranslateService } from '@ngx-translate/core';
   imports: [ParentComponent],
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
     constructor(private translate: TranslateService) {
+      
     translate.setDefaultLang('ar');
     translate.use('ar');
   }
-
+ngOnInit(): void {
+    AOS.init({
+      duration: 2000, // Animation duration
+      once: false, // Whether animation should happen only once
+      easing: 'ease-in-out', // Easing type
+      offset: 100, // Offset (in pixels) from the original trigger point
+    }); 
+  }
   title = 'Solve-Task';
 }
